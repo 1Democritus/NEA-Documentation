941cbfee2bf484ab14ffa323b1d8a13c3d59adb4
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-11-10 09:11:44 +0000

    Create nn.py

---
+++ Coding/nn.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+
+class DNN():
+  def __init__(learningRate, hiddenlayerNo):
+    self.__learningRate = learningRate
+    self.__hiddenlayerNo = hiddenlayerNo
+
+  def feedforward(tuple):
+    pass
+
+  def backprop(tuple):
+    pass
+
+  def softmax(tuple):
+    expSum = sum(exp(i) for i in tuple)
+    softmaxTuple = [exp(weight)/expSum for weight in tuple]
+    return softmaxTuple
