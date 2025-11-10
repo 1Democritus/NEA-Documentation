59b83d4692094bd57e783c33dc28acc3b28a4461
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-11-10 09:45:01 +0000

    Update nn.py

--- Coding/nn.py
+++ Coding/nn.py
@@ -20,14 +20,17 @@ class DNN():
   def backprop(self, tuple):
     pass
 
+  @static
   def softmax(tuple): #activation function to be used for final layer
     expSum = sum(numpy.exp(i) for i in tuple)
     softmaxTuple = [numpy.exp(weight)/expSum for weight in tuple]
     return softmaxTuple
 
+  @static
   def ReLU(x):
     return max(0, x)
 
+  @static
   def meanSquaredError(predicted, actual):
     return sum((actual[idx]-predicted[idx])**2 for idx in range(len(predicted)))
 
