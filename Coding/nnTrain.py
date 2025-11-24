32ffeb8465fa3605570c07a321c92f8e765d47c9
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-11-24 09:40:23 +0000

    Update nnTrain.py

--- Coding/nnTrain.py
+++ Coding/nnTrain.py
@@ -3,3 +3,9 @@ import pandas
 
 model = nn.DNN(columnSize = 2000, outputSize = 5)
 df = pandas.read_csv("disease_diagnosis.csv")
+
+#feature engineering
+def OHElabel(value):
+    conversionDict = {"Healthy": [1,0,0,0,0], "Bronchitis": [0,1,0,0,0], "Flu":[0,0,1,0,0], "Cold": [0,0,0,1,0], "Pneumonia": [0,0,0,0,1]}
+    return np.array(conversionDict[value])
+df["Diagnosis"] = df["Diagnosis"].apply(OHElabel)
