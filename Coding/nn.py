cccba2e75be5cbdcaa10ff65477f802ecfde344d
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-01 09:58:14 +0000

    Update nn.py

--- Coding/nn.py
+++ Coding/nn.py
@@ -9,15 +9,15 @@ class DNN():
     self.__w2 = numpy.random.randn(outputSize, 50) - 0.5
     self.__b2 = numpy.random.randn(outputSize, 1) - 0.5
 
-  def feedForward(self, tuple):
-    hiddenTensors = numpy.dot(self.__w1, tuple) - self.__b1
+  def feedForward(self, tupl):
+    hiddenTensors = numpy.dot(self.__w1, tupl) - self.__b1
     hiddenTensorsActivated = self.ReLU(hiddenTensors)
     finalTensors = numpy.dot(self.__w2, hiddenTensorsActivated) - self.__b2
     finalTensorsActivated = self.softmax(finalTensors)
     return hiddenTensors, hiddenTensorsActivated, finalTensors, finalTensorsActivated
   
   def backprop(self, epochError, tensors, trainset):
-    Ysize = trainset.shape(1)
+    Ysize = trainset.shape[1]
     finalWeightDerivative = numpy.dot(epochError, tensors[1].T) / Ysize
     finalBiasDerivative = numpy.sum(epochError) / Ysize
     hiddenDerivative = numpy.dot(self.__w2.T, epochError) * self.ReLUderivative(tensors[0])
@@ -32,28 +32,27 @@ class DNN():
     self.__b2 -= self.__learningRate * finalBiasDerivative
 
   @staticmethod
-  def softmax(tuple): #activation function to be used for final layer
-    expSum = numpy.sum(numpy.exp(i) for i in tuple)
-    softmaxTuple = numpy.array([numpy.exp(weight)/expSum for weight in tuple])
-    return softmaxTuple
+  def softmax(tupl): #activation function to be used for final layer
+    expSums = numpy.exp(tupl - numpy.max(tupl, axis=0, keepdims=True))
+    return expSums / numpy.sum(expSums, axis=0, keepdims=True)
 
   @staticmethod
-  def ReLU(tuple):
-    return numpy.maximum(0, tuple)
+  def ReLU(tupl):
+    return numpy.maximum(0, tupl)
 
   @staticmethod
-  def ReLUderivative(tuple):
+  def ReLUderivative(tupl):
     #needed for finding derivatives
-    return (tuple>0).astype(float)
+    return (tupl>0).astype(float)
 
 def trainModel(model, epochCount, trainset, label):
-  tuple = numpy.array(trainset).flatten()
-  print(tuple)
+  tupl = trainset
   for epoch in range(epochCount):
-    tensors = model.feedForward(tuple)
-    predictions = numpy.argmax(numpy.vstack([tensors[3][0], tensors[3][1], tensors[3][2], tensors[3][3], tensors[3][4]]), axis = 0)
-    epochError = predictions - label
-    hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative = model.backprop(epochError, tensors, tuple)
+    tensors = model.feedForward(tupl)
+    predictions = numpy.argmax(tensors[3], axis = 0)
+    #find crossentropy error
+    epochError = tensors[3] - label
+    hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative = model.backprop(epochError, tensors, tupl)
     model.updateParameters(hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative)
     accuracy = numpy.sum(predictions == label)/len(label)
     print(f"Epoch: {epoch + 1}, accuracy: {accuracy}")
