634f73ca6b56c07abcb928d6cafe1ef28c1d6803
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-08 10:06:57 +0000

    Update nnTrain.py

--- Coding/nnTrain.py
+++ Coding/nnTrain.py
@@ -15,12 +15,17 @@ def convertGender(value):
 
 df[["Systolic_Pressure", "Diastolic_Pressure"]] = df["Blood_Pressure_mmHg"].str.split('/', n = 1, expand = True)
 df["Gender"] = df["Gender"].apply(convertGender)
-#combine symptom columns into 1
+#combine symptom columns into 1 then separate into list
 df["Symptoms"] = df[["Symptom_1", "Symptom_2", "Symptom_3"]].apply(lambda val: ",".join(val.values.astype(str)), axis = 1)
+df["Symptoms"] = df["Symptoms"].apply(lambda val: val.split(","))
+symptomList = ["Body ache", "Cough", "Shortness of breath", "Fatigue", "Fever", "Headache", "Runny nose", "Sore throat"]
+for symptom in symptomList:
+    df[symptom] = [[symptom]] * len(df)
+    df[symptom] = df.apply(lambda row: int(row[symptom] in row["Symptoms"]), axis = 1)
 
 
-df = df.drop(columns = ["Symptom_1", "Symptom_2", "Symptom_3", "Treatment_Plan", "Severity", "Blood_Pressure_mmHg"])
+df = df.drop(columns = ["Symptoms","Symptom_1", "Symptom_2", "Symptom_3", "Treatment_Plan", "Severity", "Blood_Pressure_mmHg"])
 labels = np.stack(df["Diagnosis"].apply(OHElabel).values).T
 features = df.drop(columns = ["Diagnosis"]).to_numpy(dtype = float).T
 model = nn.DNN(learningRate = 0.01, columnSize = features.shape[0], outputSize = labels.shape[0])
-predictorModel = nn.trainModel(model = model, epochCount = 300, label = labels, trainset = features)
+predictorModel = nn.trainModel(model = model, epochCount = 200, label = labels, trainset = features)
