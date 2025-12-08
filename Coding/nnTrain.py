1deb9d640d7a193da594ab6fedb28707a4c510c1
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-08 09:44:17 +0000

    Update nnTrain.py

--- Coding/nnTrain.py
+++ Coding/nnTrain.py
@@ -5,6 +5,7 @@ import pandas
 
 df = pandas.read_csv("disease_diagnosis.csv")
 
+#convert label from qualitative to one-hot-encoding
 def OHElabel(value):
     conversionDict = {"Healthy": [1,0,0,0,0], "Bronchitis": [0,1,0,0,0], "Flu":[0,0,1,0,0], "Cold": [0,0,0,1,0], "Pneumonia": [0,0,0,0,1]}
     return conversionDict[value]
@@ -14,6 +15,8 @@ def convertGender(value):
 
 df[["Systolic_Pressure", "Diastolic_Pressure"]] = df["Blood_Pressure_mmHg"].str.split('/', n = 1, expand = True)
 df["Gender"] = df["Gender"].apply(convertGender)
+#combine symptom columns into 1
+df["Symptoms"] = df[["Symptom_1", "Symptom_2", "Symptom_3"]].apply(lambda val: ",".join(val.values.astype(str)), axis = 1)
 
 
 df = df.drop(columns = ["Symptom_1", "Symptom_2", "Symptom_3", "Treatment_Plan", "Severity", "Blood_Pressure_mmHg"])
