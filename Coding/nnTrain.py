0158a3c6956da2d9c5d0e593662f96af46414cc5
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-08 09:35:00 +0000

    Update nnTrain.py

--- Coding/nnTrain.py
+++ Coding/nnTrain.py
@@ -4,16 +4,20 @@ import pandas
 
 
 df = pandas.read_csv("disease_diagnosis.csv")
-#for now, drop these columns for easy demo. Later on, plan to split blood pressure to systolic and diastolic
-df = df.drop(columns = ["Gender", "Symptom_1", "Symptom_2", "Symptom_3", "Treatment_Plan", "Severity", "Blood_Pressure_mmHg"])
 
-#convert label to one-hot-encoding for later operations
 def OHElabel(value):
     conversionDict = {"Healthy": [1,0,0,0,0], "Bronchitis": [0,1,0,0,0], "Flu":[0,0,1,0,0], "Cold": [0,0,0,1,0], "Pneumonia": [0,0,0,0,1]}
     return conversionDict[value]
+
+def convertGender(value):
+    return 0 if value == "Male" else 1
+
+df[["Systolic_Pressure", "Diastolic_Pressure"]] = df["Blood_Pressure_mmHg"].str.split('/', n = 1, expand = True)
+df["Gender"] = df["Gender"].apply(convertGender)
+
+
+df = df.drop(columns = ["Symptom_1", "Symptom_2", "Symptom_3", "Treatment_Plan", "Severity", "Blood_Pressure_mmHg"])
 labels = np.stack(df["Diagnosis"].apply(OHElabel).values).T
 features = df.drop(columns = ["Diagnosis"]).to_numpy(dtype = float).T
 model = nn.DNN(learningRate = 0.01, columnSize = features.shape[0], outputSize = labels.shape[0])
-
-#define final model to be used for predictions
 predictorModel = nn.trainModel(model = model, epochCount = 300, label = labels, trainset = features)
