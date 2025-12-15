d80e18e1bd1e7f0e0daddb20120e5acbc0a698f4
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-15 14:26:10 +0000

    Update GUI.py

--- Coding/GUI.py
+++ Coding/GUI.py
@@ -1,1 +1,72 @@
 from tkinter import *
+from nnTrain import predictorModel as Oracle
+import string
+
+#setting up accounts list for login validation
+accountFile = open("accountsDemo.txt", "a")
+accountDetails = []
+for line in accountFile.readlines():
+    accountDetails.append(line.split(" "))
+print(accountDetails)
+
+class Interface:
+    #open main menu of interface
+    def __init__(self):
+        self.screen = Tk()
+        self.screen.geometry("500x500")
+        self.screen.state("zoomed")
+        self.screen.title("interfacePOC")
+
+        self.registryLabel = Label(self.screen, text = "Welcome back! Please sign in or create a new account so that we can help with your health concerns")
+        self.registryLabel.pack()
+        self.loginButton = Button(self.screen, text = "SIGN IN", command = self.login)
+        self.loginButton.pack()
+        self.registerButton = Button(self.screen, text = "REGISTER", command = self.register)
+        self.registerButton.pack()
+
+    #function for transitioning between screens
+    def clearScreen(self):
+        for widget in self.screen.winfo_children():
+            widget.destroy()
+
+    def login(self):
+        self.clearScreen()
+        #implement account checking
+    
+    def register(self):
+        self.clearScreen()
+        #implement strong password checker
+
+#general functions that don't need parameters
+def strongPasswordChecker(password):
+    checklist = [False, False, False, False, False]
+    if len(password) >= 8:
+        checklist[0] = True
+    for k in password:
+        try:
+            k = int(k)
+            checklist[1] = True
+        except:
+            pass
+        if k in string.ascii_uppercase:
+            checklist[3] = True
+        elif k in string.ascii_lowercase:
+            checklist[2] = True
+        elif k in ["?", "!", "$", "£", "#", "@", "(", ")", "*", ".", "%"]:
+            checklist[4] = True
+        return False not in checklist
+    
+def validEmailChecker(email):
+    validDomains = ["nhs.ac.uk", ""]
+    emailContents = email.split("@")
+    if len(emailContents) != 2:
+        return False
+    name, domain = emailContents[0], emailContents[1]
+    for disallowedChar in ["/", "?", "!", "*", "(", ")"]:
+        if disallowedChar in name:
+            return False
+    return domain in validDomains and name[0] not in [".", "_", "-"]
+        
+
+nhsInterface = Interface()
+nhsInterface.screen.mainloop()
