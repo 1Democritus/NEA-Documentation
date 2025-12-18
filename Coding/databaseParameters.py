c2f5b847be21e8631db50b917ac3b0e68daaea43
1Democritus <138218435+1Democritus@users.noreply.github.com>
2025-12-18 11:28:17 +0000

    Update databaseParameters.py

--- Coding/databaseParameters.py
+++ Coding/databaseParameters.py
@@ -1,2 +1,2 @@
 #can just import this variable into other files instead of writing them out every time the code connects to postgreSQL
-databaseParameters = {'user':'postgres', 'password': '!clun25K', 'port': '5432', 'host': 'localhost'}
+PARAMETERS = {'user':'postgres', 'password': '!clun25K', 'port': '5432', 'host': 'localhost'}
