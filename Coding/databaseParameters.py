76c1e30af2e0f56da900d57b1d00b2eab114b820
1Democritus <138218435+1Democritus@users.noreply.github.com>
2026-01-26 10:08:17 +0000

    Delete Coding/databaseParameters.py

--- Coding/databaseParameters.py
+++
@@ -1,2 +0,0 @@
-#can just import this variable into other files instead of writing them out every time the code connects to postgreSQL
-PARAMETERS = {'user':'postgres', 'password': '!clun25K', 'port': '5432', 'host': 'localhost'}
