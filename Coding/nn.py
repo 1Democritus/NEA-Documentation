a3d249fbc5218c8ce1d9447719079030909294d1
1Democritus <138218435+1Democritus@users.noreply.github.com>
2026-02-09 10:00:35 +0000

    Update nn.py

--- Coding/nn.py
+++ Coding/nn.py
@@ -3,20 +3,19 @@ import numpy
 class DNN():
   def __init__(self, learningRate, columnSize, outputSize):
     self.__learningRate = learningRate #determines the rate at which weights change
-    #initialise random values between 0.5 and -0.5
-    self.__w1 = numpy.random.randn(50, columnSize) * numpy.sqrt(2 / columnSize)
-    self.__b1 = numpy.zeros((50,1))
+    self.__w1 = numpy.random.randn(50, columnSize) * numpy.sqrt(2 / columnSize) #for scaling the inputs
+    self.__b1 = numpy.zeros((50, 1))
     self.__w2 = numpy.random.randn(outputSize, 50) * numpy.sqrt(2 / 50)
     self.__b2 = numpy.zeros((outputSize, 1))
 
   def feedForward(self, tupl):
-    hiddenTensors = numpy.dot(self.__w1, tupl) + self.__b1
-    hiddenTensorsActivated = self.ReLU(hiddenTensors)
+    hiddenTensors = numpy.dot(self.__w1, tupl) + self.__b1 #dot function is a shortcut to multiplying all neuron weights by all inputs and storing the results
+    hiddenTensorsActivated = self.ReLU(hiddenTensors) #pass tensors through activation function for nonlinearity
     finalTensors = numpy.dot(self.__w2, hiddenTensorsActivated) + self.__b2
     finalTensorsActivated = self.softmax(finalTensors)
     return hiddenTensors, hiddenTensorsActivated, finalTensors, finalTensorsActivated
   
-  def backprop(self, epochError, tensors, trainset):
+  def backprop(self, epochError, tensors, trainset): #calculating partial derivatives of functions with respect to the loss
     Ysize = trainset.shape[1]
     finalWeightDerivative = numpy.dot(epochError, tensors[1].T) / Ysize
     finalBiasDerivative = numpy.sum(epochError, axis = 1, keepdims = True) / Ysize
@@ -26,6 +25,7 @@ class DNN():
     return hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative
 
   def updateParameters(self, hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative):
+    #subtract derivative multiplied by learning rate from variables
     self.__w1 -= self.__learningRate * hiddenWeightDerivative
     self.__b1 -= self.__learningRate * hiddenBiasDerivative
     self.__w2 -= self.__learningRate * finalWeightDerivative
@@ -46,12 +46,12 @@ class DNN():
     return (tupl>0).astype(float)
 
 def trainModel(model, epochCount, trainset, label):
-  tupl = trainset
   for epoch in range(epochCount):
-    tensors = model.feedForward(tupl)
+    tensors = model.feedForward(trainset)
     predictions = numpy.argmax(tensors[3], axis = 0)
+    #I've chosen MAE (mean absolute error) to use for backpropagation
     epochError = tensors[3] - label
-    hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative = model.backprop(epochError, tensors, tupl)
+    hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative = model.backprop(epochError, tensors, trainset)
     model.updateParameters(hiddenWeightDerivative, hiddenBiasDerivative, finalWeightDerivative, finalBiasDerivative)
     accuracy = numpy.sum(predictions == numpy.argmax(label, axis = 0))/label.shape[1]
     print(f"Epoch: {epoch + 1}, accuracy: {accuracy}")
