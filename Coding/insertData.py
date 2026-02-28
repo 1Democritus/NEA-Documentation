d20c5952fc1e8dc76f6deea9c444fb867857aa47
1Democritus <138218435+1Democritus@users.noreply.github.com>
2026-02-28 14:52:28 +0000

    Update insertData.py

--- Coding/insertData.py
+++ Coding/insertData.py
@@ -1,1 +1,24 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv(override=True)
+PARAMETERS = os.getenv("PARAMETERS").split(",") #contains details needed to access the server
+PARAMETERS = {"host": PARAMETERS[2], "user": PARAMETERS[0], "password": PARAMETERS[1]}
+connection = psycopg2.connect(dbname = 'postgres', **PARAMETERS)
+connection.autocommit = True #every SQL query will be committed without requiring specific commits
+cursor = connection.cursor()
 
+def rollingHash(key, rollingPrime = 31, length = 101):
+        hashSum = 0
+        for i in range(len(key)):
+            hashSum += ord(key[i]) * (rollingPrime ** i)
+        return hashSum % length
+
+accounts = [('c.akcabay@denizati.tr', 'boSSdhuraN', ''), ('mike.smith@nhs.ac.uk', 'mike1234', 'AP6QT#')]
+patient = [('CemAkc001','Cem', 'Akcabay', '')]
+for email, password, accessCode in accounts:
+  password = rollingHash(password)
+  accessCode = rollingHash(accessCode) if accessCode != '' else ''
+  statement = 'INSERT INTO loginDetails VALUES(%s, %s, %s)'
+  cursor.execute(statement, (email, password, accessCode))
+conn.close()
